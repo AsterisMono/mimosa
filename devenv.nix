a8f5a0f3c97e238f257064c5f751ab819b218e8d
Noa Virellia <noa@requiem.garden>
2026-01-11 21:10:25 +0800

    feat(terraform): use terraform to start talos cluster
    Signed-off-by: Noa Virellia <noa@requiem.garden>

---
+++ devenv.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  packages = with pkgs; [
+    kubeseal
+    kubectl
+    kubeconform
+    kustomize
+    kubernetes-helm
+    argocd
+    terraform
+    packer
+    talosctl
+    just
+    _1password-cli
+  ];
+
+  env = {
+    KUBECONFIG = "./.kube/kubeconfig";
+    TALOSCONFIG = "./.kube/talosconfig";
+    TF_VAR_hcloud_token = "op://Requiem.Garden/Hetzner.Token.API.RequiemGarden/credential";
+    AWS_ACCESS_KEY_ID = "op://Requiem.Garden/Hetzner.Token.S3.MimosaTerraformState/username";
+    AWS_SECRET_ACCESS_KEY = "op://Requiem.Garden/Hetzner.Token.S3.MimosaTerraformState/credential";
+  };
+}
---
+++ terraform/kubernetes.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "s3" {
+    bucket = "mimosa-terraform-state"
+    key    = "mimosa-cluster.tfstate"
+    region = "eu-central-1"
+
+    endpoints = {
+      s3 = "https://fsn1.your-objectstorage.com"
+    }
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+  }
+}
+
+variable "hcloud_token" {
+  type        = string
+  description = "Hetzner Cloud API Token"
+  sensitive   = true
+}
+
+module "kubernetes" {
+  source  = "hcloud-k8s/kubernetes/hcloud"
+  version = "3.19.0"
+
+  cluster_name = "mimosa"
+  hcloud_token = var.hcloud_token
+
+  cluster_kubeconfig_path  = "kubeconfig"
+  cluster_talosconfig_path = "talosconfig"
+
+  cert_manager_enabled       = true
+  cilium_gateway_api_enabled = true
+
+  # Do not auto detect current operator IP
+  firewall_use_current_ipv4 = false
+  firewall_use_current_ipv6 = false
+
+  firewall_api_source = [
+    "103.151.172.24/32", # HKZ03
+    "101.88.92.62/32"    # Asai Home
+  ]
+
+  kube_api_load_balancer_enabled = true
+  kube_api_hostname              = "mimosa.requiem.garden"
+
+  control_plane_nodepools = [
+    { name = "control", type = "cpx22", location = "fsn1", count = 3 }
+  ]
+  worker_nodepools = [
+    { name = "worker", type = "cpx22", location = "fsn1", count = 3 }
+  ]
+}
